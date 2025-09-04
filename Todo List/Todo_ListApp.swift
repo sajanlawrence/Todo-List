9669efa92dbd1d410153e5216bbce0ec582e3d3e
Sajan Lawrence <sajanlawrence12345@gmail.com>
2025-09-04 13:36:39 +0530

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Sajan Lawrence on 04/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
